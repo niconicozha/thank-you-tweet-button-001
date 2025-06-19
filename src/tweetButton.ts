2562ff9a297a2d31c12001a3733ab8339f93caee
李树钊 <niconicozha@gmail.com>
2025-06-19 22:21:54 +0800

    fix(tweetbutton): change send text to post

--- src/tweetButton.ts
+++ src/tweetButton.ts
@@ -11,7 +11,7 @@ function showError() {
   const errorContainer = document.createElement("div");
   const errorParagraph = document.createElement("p");
   const errorParagraphText = document.createTextNode(
-    "❌ Error: 'rating' parameter is missing in 'tweetButtonParameters'."
+    "❌ Error: One or more required parameters are missing in 'tweetButtonParameters'."
   );
   errorContainer.setAttribute(
     "style",
@@ -34,7 +34,7 @@ export function tweetButton(
   const tweetButtonContainer = document.createElement("section");
   const anchorElement = document.createElement("a");
   const anchorElementContent = document.createTextNode(
-    "Send a thank you tweet"
+    "Post a thank you tweet"
   );
   const tweetUrl = `https://twitter.com/intent/tweet?text=Thank+you,+
   %40oluwatobiss.+Your+book+helped+me+create,+test,+and+publish+${
