aa632027a03d959068700b05a8d6c9019253d812
李树钊 <niconicozha@gmail.com>
2025-06-19 21:51:53 +0800

    feat: make users provide a rating
    BREAKING CHANGE: This commit breaks compatibility with the previous version

--- src/__tests__/tweetButton.test.ts
+++ src/__tests__/tweetButton.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@jest/globals";
 import { tweetButton } from "../tweetButton";
 
 test("tweetButton returns a truthy value", () => {
-  expect(tweetButton()).toBeTruthy();
+  expect(tweetButton(5, 17)).toBeTruthy();
 });
--- src/tweetButton.ts
+++ src/tweetButton.ts
@@ -1,14 +1,45 @@
-export function tweetButton(quantity?: number) {
+function generateStarIcons(rating: number) {
+  const megaStars = `⭐⭐⭐⭐⭐ x ${Math.round(rating / 5)}`;
+  let stars = "";
+  for (let i = 0; i < rating; i++) {
+    stars += "⭐";
+  }
+  return rating > 10 ? megaStars : stars;
+}
+
+function showError() {
+  const errorContainer = document.createElement("div");
+  const errorParagraph = document.createElement("p");
+  const errorParagraphText = document.createTextNode(
+    "❌ Error: 'rating' parameter is missing in 'tweetButtonParameters'."
+  );
+  errorContainer.setAttribute(
+    "style",
+    "display: flex; justify-content: center; margin-top: 30px"
+  );
+  errorParagraph.setAttribute(
+    "style",
+    "background-color: #DC3545; color: #fff; width: 35%; border-radius: 5px; padding: 15px 20px"
+  );
+  errorParagraph.appendChild(errorParagraphText);
+  errorContainer.appendChild(errorParagraph);
+  return errorContainer;
+}
+
+export function tweetButton(rating: number, quantity?: number) {
   const tweetButtonContainer = document.createElement("section");
   const anchorElement = document.createElement("a");
   const anchorElementContent = document.createTextNode(
     "Send a thank you tweet"
   );
-  const tweetUrl = `https://twitter.com/intent/tweet?text=Thank+you,+%40oluwatobiss.+Your+book+helped+me+create,+test,+and+publish+${
+  const tweetUrl = `https://twitter.com/intent/tweet?text=Thank+you,+
+  %40oluwatobiss.+Your+book+helped+me+create,+test,+and+publish+${
     quantity && quantity > 1 ? quantity : "an"
   }+NPM+${
     quantity && quantity > 1 ? "packages" : "package"
-  }%0A%0ACreating%20NPM%20Package%0A%0Ahttps%3A%2F%2Famzn.to/4lifL3n`;
+  }.%0A%0ABook's+Rating:+${rating}-star+rating!+${generateStarIcons(
+    rating
+  )}%0A%0Ahttps%3A%2F%2Famzn.to/4lifL3n`;
 
   tweetButtonContainer.setAttribute("id", "tweet-btn-container");
   anchorElement.setAttribute("class", "tweet-button");
@@ -18,5 +49,5 @@ export function tweetButton(quantity?: number) {
   anchorElement.appendChild(anchorElementContent);
   tweetButtonContainer.appendChild(anchorElement);
 
-  return tweetButtonContainer;
+  return typeof rating === "number" ? tweetButtonContainer : showError();
 }
